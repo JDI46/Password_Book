bbe1d8cd5d86b1d8f38ac80e8f8dcfcdebfdda07
Xander <xwhitby20@gmail.com>
2023-12-26 18:31:06 -0700

    My new edits

--- password_verifer.py
+++ password_verifer.py
@@ -2,8 +2,18 @@ def make_password(password_prompt, list):
     new_password = list.append(password_prompt)
     return (new_password)
 
+def password_verification(verify,  list):
+    if verify in list:
+        print('This is Correct')
+    else:
+        print('Please Try again')
+    
+
 create_new_password = input('Create a new password: ')
 password_keeper = []
 password_generator = make_password(create_new_password, password_keeper)
 
-print(password_keeper)
+verify_password = input('What is your password? ')
+check_password = password_verification(verify_password, password_keeper)
+password_keeper = []
+verified = 'Yes'
