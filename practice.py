3ef846a2b0b1a20ee03a3528766aad733fcc047f
Xander <xwhitby20@gmail.com>
2023-12-26 18:25:21 -0700

    verification added

---
+++ practice.py
@@ -0,0 +1,24 @@
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+    
+#     #making a new method
+
+#     def say_hello(self):
+#         print(f'Hello, {self.name}')
+    
+#     #using a setter method to set or mutate value of a an attribute in a class
+
+#     def set_age(self, age):
+#         self.age = age
+#     # I don't need to do this but I can use a getter method and here is what it looks like
+    
+#     def get_age(self):
+#         return self.age
+    
+# p1 = Person('Xander')
+# p1.set_age(21)
+# p1.say_hello()
+
+# print(p1.age)
+
